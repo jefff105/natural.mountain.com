2a88143bf5a093a4f638cb1876242ae25680a461
jefff105 <103900106+jefff105@users.noreply.github.com>
2022-04-17 22:07:18 +0300

    Add files via upload

---
+++ untitled23/src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+ import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+	  Scanner input = new Scanner(System.in);
+	  char license ;
+	  int number;
+	  System.out.print("enter the license: ");
+	  license= input.char();
+    }
+}
